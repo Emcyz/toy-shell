6d994efd157605f4d58464ba5603281e535a881b
Emcyz <comicplayer@naver.com>
2021-05-15 20:10:34 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -16,42 +16,31 @@ int main(void)
     pid_t pid, cpid;
     
     while (true) {
-        char *s;
-        int len;
         
-        printf("MyShell $ ");
-        s = fgets(command, MAX_LEN_LINE, stdin);
-        if (s == NULL) {
-            fprintf(stderr, "fgets failed\n");
-            exit(1);
-        }
-        
-        len = strlen(command);
-        printf("%d\n", len);
-        if (command[len - 1] == '\n') {
-            command[len - 1] = '\0'; 
-        }
-        
-        printf("[%s]\n", command);
-      
         pid = fork();
-        if (pid < 0) {
+        if (pid < 0)
+        { 
             fprintf(stderr, "fork failed\n");
             exit(1);
         } 
-        if (pid != 0) {  /* parent */
+        if (pid != 0)
+        {  /* parent */
             cpid = waitpid(pid, &status, 0);
-            if (cpid != pid) {
+            if (cpid != pid)
+            {
                 fprintf(stderr, "waitpid failed\n");        
             }
             printf("Child process terminated\n");
-            if (WIFEXITED(status)) {
+            if (WIFEXITED(status))
+            {
                 printf("Exit status is %d\n", WEXITSTATUS(status)); 
             }
         }
-        else {  /* child */
+        else
+        {  /* child */
             ret = execve(args[0], args, NULL);
-            if (ret < 0) {
+            if (ret < 0)
+            {
                 fprintf(stderr, "execve failed\n");   
                 return 1;
             }
